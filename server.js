7bdafa52daeb687d8471951f9420a8e11e5339a2
System <none>
2017-10-12 15:37:45 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -8,6 +8,7 @@
 var fs = require('fs');
 var express = require('express');
 var app = express();
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
 if (!process.env.DISABLE_XORIGIN) {
   app.use(function(req, res, next) {
@@ -37,6 +38,51 @@ app.route('/')
     .get(function(req, res) {
 		  res.sendFile(process.cwd() + '/views/index.html');
     });
+
+app.get("/latest", function (request, response) {
+    response.writeHead(200, { 'Content-Type': 'application/json' });
+    response.end(JSON.stringify(queryCatalogue));
+})
+
+app.get("/imagesearch/:query", function (request, response) {
+  var page,
+      time = new Date();
+  if (request.query.offset == 0 || request.query.offset == null) {
+    return page = 1;
+  } else if (request.query.offset < 0) {
+    return page = 1;
+  } else if (request.query.offset == Math.abs(request.query.offset)) {
+    return page = request.query.offset + 1;
+  } else {
+    response.end("Offset entry is invalid. Please type a number or leave it blank.");
+  };
+  queryCatalogue.push({
+    term: request.params.query,
+    page: page,
+    when: time.toString()
+  });
+  var url = "https://www.googleapis.com/customsearch/v1?q=" + request.params.query +"&cx=" + process.env.CSE_ID + "&key=" + process.env.API_KEY +
+             "&searchType=image&start=" + page,
+      xhr = new XMLHttpRequest(),
+      packet = [];
+  xhr.open("GET", url);
+  xhr.send();
+  xhr.onload = function() {
+    var json = JSON.parse(xhr.responseText).items;
+    json.forEach(function (item) {
+      packet.push({
+        url: item.link,
+        snippet: item.snippet,
+        thumbnail: item.image.thumbnailLink,
+        context: item.image.contextLink
+      });
+    });
+    response.writeHead(200, { 'Content-Type': 'application/json' });
+    response.end(JSON.stringify(packet));
+  };
+});
+
+var queryCatalogue = []; 
 
 // Respond not found to all the wrong routes
 app.use(function(req, res, next){
