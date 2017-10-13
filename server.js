a287043c4ec5061d4b233ee675548ec96cb3a9ac
System <none>
2017-10-13 04:40:19 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -39,53 +39,53 @@ app.route('/')
 		  res.sendFile(process.cwd() + '/views/index.html');
     });
 
-app.get("/latest", function (request, response) {
-    response.writeHead(200, { 'Content-Type': 'application/json' });
+app.get("/latest", function(request, response) {
+    response.writeHead(200, {'Content-Type': 'application/json'});
     response.end(JSON.stringify(queryCatalogue));
 })
 
-app.get("/imagesearch/:query", function (request, response) {
+app.get("/imagesearch/:query", function(request, response) {
   var page,
-      time = new Date();
-  if (request.query.offset == 0 || request.query.offset == null) {
-    return page = 1;
-  } else if (request.query.offset < 0) {
-    return page = 1;
-  } else if (request.query.offset == Math.abs(request.query.offset)) {
-    return page = request.query.offset + 1;
+      time = new Date(),
+      xhr = new XMLHttpRequest(),
+      packet = [];
+  if (request.query.offset <= 0 || request.query.offset == null) {
+    page = 1;
+  } else if (request.query.offset > 0) {
+    page = Number(request.query.offset) + 1;
   } else {
     response.end("Offset entry is invalid. Please type a number or leave it blank.");
   };
-  queryCatalogue.push({
-    term: request.params.query,
-    page: page,
-    when: time.toString()
-  });
   var url = "https://www.googleapis.com/customsearch/v1?q=" + request.params.query +"&cx=" + process.env.CSE_ID + "&key=" + process.env.API_KEY +
-             "&searchType=image&start=" + page,
-      xhr = new XMLHttpRequest(),
-      packet = [];
-  xhr.open("GET", url);
-  xhr.send();
-  xhr.onload = function() {
-    var json = JSON.parse(xhr.responseText).items;
-    json.forEach(function (item) {
-      packet.push({
-        url: item.link,
-        snippet: item.snippet,
-        thumbnail: item.image.thumbnailLink,
-        context: item.image.contextLink
-      });
+             "&searchType=image&start=" + page;
+  if (page !== undefined) {
+    queryCatalogue.push({
+      term: request.params.query,
+      page: page,
+      when: time.toString()
     });
-    response.writeHead(200, { 'Content-Type': 'application/json' });
-    response.end(JSON.stringify(packet));
+    xhr.open("GET", url);
+    xhr.send();
+    xhr.onload = function() {
+      var json = JSON.parse(xhr.responseText).items;
+      json.forEach(function(item) {
+        packet.push({
+          url: item.link,
+          snippet: item.snippet,
+          thumbnail: item.image.thumbnailLink,
+          context: item.image.contextLink
+        });
+      });
+      response.writeHead(200, {'Content-Type': 'application/json'});
+      response.end(JSON.stringify(packet));
+    };
   };
 });
 
 var queryCatalogue = []; 
 
 // Respond not found to all the wrong routes
-app.use(function(req, res, next){
+app.use(function(req, res, next) {
   res.status(404);
   res.type('txt').send('error in image query');
 });
@@ -99,6 +99,6 @@ app.use(function(err, req, res, next) {
   }  
 })
 
-app.listen(process.env.PORT, function () {
+app.listen(process.env.PORT, function() {
   console.log('Node.js listening ...');
 });
